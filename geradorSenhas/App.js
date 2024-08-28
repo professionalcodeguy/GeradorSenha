20c66bfb8fc982c8675e40e2426123d562802b17
professionalcodeguy <kaue.g.p2015@gmail.com>
2024-08-28 17:55:50 +0000

    sfasf

---
+++ geradorSenhas/App.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+ 
+let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ 
+export default function App() {
+  const [senhaGerada, setSenhaGerada] = useState("")
+ 
+  function gerarSenha(){
+   
+    let senha = "";
+ 
+    for (let i = 0, n = charset.length; i < 10; i++){
+      senha += charset.charAt(Math.floor(Math.random() * n));
+    }
+ 
+    setSenhaGerada(senha)
+ 
+  }
+ 
+ 
+  return (    
+    <View style={styles.container}>
+      <Image
+        source={require("./src/img/logolock.png")}
+        style={styles.logo}
+      />
+ 
+      <Text style={styles.title}> LockGen </Text>
+ 
+      <TouchableOpacity style={styles.button} onPress={gerarSenha}>
+        <Text style={styles.textButton}> Gerar Senha </Text>
+      </TouchableOpacity>
+ 
+     
+      <Text style={styles.senha}> {senhaGerada} </Text>
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 28,
+    marginBottom: 50,
+  },
+  button: {
+    backgroundColor: '#333',
+    width: '70%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    padding: 6,    
+  },
+  textButton:{
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  senha: {
+    marginTop: 20,
+    color: '#333',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
